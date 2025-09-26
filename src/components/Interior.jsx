5837708e0a72e1a2b829e177b53a46ffa30c54e2
kalidass26 <mathesh45362@gmail.com>
2025-09-26 11:36:29 +0530

    Commit other changes before adding Interior.css

--- src/components/Interior.jsx
+++ src/components/Interior.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./Interior.css"; // custom styles go here
+import "./Interior.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
